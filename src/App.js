aba9da73013dfbc6aeea295676b3b56ca1a7e4eb
LminWoo99 <mw310@naver.com>
2023-08-07 17:01:38 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+//switch는 route를 찾음(url을 찾음)
+//Link는 브라우저 새로고침 없이도 유저를 다른 페이지로 이동시켜주는 컴포턴트
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return (<Router>
+        <Routes> 
+          <Route path="/movie/:id" element={<Detail />}/>
+          <Route path="/" element={<Home />} />
+        
+        </Routes>
+  </Router>);
+          }
 
 export default App;
